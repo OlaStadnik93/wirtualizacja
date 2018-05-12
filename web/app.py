8cde0b965cb086f6ebf955e0d4592978a06e2d37
OlaStadnik93 <olastadnik93@gmail.com>
2018-05-12 09:20:43 +0000

    tam cos

---
+++ web/app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+from flask import render_template
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+  return "It works YAY!!"
+
+@app.route("/order-animation")
+def order_animation():
+  return render_template(
+    "order.html",
+    invitation="welcome at my creator"
+ )
+
+@app.route('/upload')
+def upload():
+    if 'my_file' not in request.files:
+        return "sth is no yes"
+    upl_file = request.files['my_file']
+    storage = new S3Storage(os.get, s3)
+    storage.save(path="", body= upl_file)
+
+if __name__ == '__main__':
+  app.run(host="0.0.0.0", port=8080, debug=True)
